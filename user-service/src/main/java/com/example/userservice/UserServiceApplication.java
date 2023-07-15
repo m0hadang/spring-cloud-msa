7bc31ec056e0f2d05677a7fd56abc72fb8ea09b2
mohadang <ehdlaos@gmail.com>
2023-07-15 17:20:35 +0900

    유레카 클라이언트 등록

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//유레카 클라이언트 등록
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
