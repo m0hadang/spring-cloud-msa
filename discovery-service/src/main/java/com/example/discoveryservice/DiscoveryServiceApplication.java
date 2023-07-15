da9d6982591d30bd0374a3eeea6fa751953b398a
mohadang <ehdlaos@gmail.com>
2023-07-15 16:54:09 +0900

    유레카 기본 설정

--- discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//유레카 서버 역할 지정
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
