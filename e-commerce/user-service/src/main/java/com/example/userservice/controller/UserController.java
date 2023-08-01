734ad43c4878e820f850c8cba60dd536821f6c0e
mohadang <ehdlaos@gmail.com>
2023-08-01 16:13:59 +0900

    user-service 라우터 정보 수정

--- e-commerce/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ e-commerce/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user-service/")
 public class UserController {
     private Environment env;
     private UserService userService;
