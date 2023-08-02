868b4d921b254cbe2856b0ed7452e001dc57415e
mohadang <ehdlaos@gmail.com>
2023-08-02 14:12:24 +0900

    config service 구현
    - 설정 파일만을 전문으로 관리하는 서비스
    - 설정 파일들의 중앙 집중화

--- e-commerce/user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ e-commerce/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -34,8 +34,12 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's Working is User Service ib PORT %s",
-                env.getProperty("local.server.port"));
+        return String.format("It's Working is User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time")
+        );
     }
 
     @GetMapping("/welcom")
